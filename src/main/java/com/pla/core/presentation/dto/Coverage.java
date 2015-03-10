dbefa2659e45c397bfbec183511d64aaa7d2a54b
sameena-waida <sameena_waida@nthdimenzion.com>
2015-03-10 13:22:48 +0530

    Coverage Feature UI committed

--- src/main/java/com/pla/core/presentation/dto/Coverage.java
+++ src/main/java/com/pla/core/presentation/dto/Coverage.java
@@ -7,5 +7,18 @@
  */
 package com.pla.core.presentation.dto;
 
+
+import com.google.common.collect.Lists;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+
 public class Coverage {
+    private String coverageName;
+    private String description;
+    List benefits = Lists.newArrayList("Death Benefit", "Accidental Death Benefit", "CI Benefit");
+    private List<String> selectedBenefits = Lists.newArrayList();
+
 }
