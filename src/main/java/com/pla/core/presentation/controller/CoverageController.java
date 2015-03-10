d91487a60c48d7a59a3b285ba30b0e018b25e293
sameena-waida <sameena_waida@nthdimenzion.com>
2015-03-10 12:16:55 +0530

    Coverage Feature UI Added

---
+++ src/main/java/com/pla/core/presentation/controller/CoverageController.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tejeswar
+ * Date: 3/9/15
+ * Time: 10:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+package com.pla.core.presentation.controller;
+
+public class CoverageController {
+}
