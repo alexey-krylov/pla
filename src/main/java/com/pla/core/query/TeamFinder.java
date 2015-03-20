fe1fddc41172cfc47e8259c256e1b9bcba5dda33
nischitharamanna12 <nischitha_ramanna@nthdimenzion.com>
2015-03-20 16:07:33 +0530

    Create team data set added

--- src/main/java/com/pla/core/query/TeamFinder.java
+++ src/main/java/com/pla/core/query/TeamFinder.java
@@ -38,9 +38,12 @@ public class TeamFinder {
 
     public static final String ACTIVE_BENEFIT_COUNT_BY_TEAM_CODE = "select count(team_id) from team where team_code=:teamCode";
 
-    public static final String findAllTeam = "SELECT tm.team_id AS teamId,tm.team_name AS teamName,b.branch as branchName,r.regional_manager as regionalManager,r.REGION_CODE AS regionCode,b.BRANCH_CODE AS branchCode FROM team tm " +
+    public static final String findAllTeam = "SELECT tm.team_id AS teamId,tm.team_name AS teamName,tm.team_code as teamCode,tf.first_Name AS firstName," +
+            "tf.last_Name as lastName,tf.from_date AS fromDate,b.branch as branchName,r.regional_manager as regionalManager,r.region AS regionName,b.BRANCH_CODE AS branchCode " +
+            "FROM team tm " +
             "LEFT JOIN region r ON r.region_code = tm.region_code " +
-            "LEFT JOIN branch b ON b.branch_code = tm.branch_code";
+            "LEFT JOIN branch b ON b.branch_code = tm.branch_code " +
+            "LEFT JOIN team_team_leader_fulfillment tf ON tf.employee_id = tm.current_team_leader";
 
 
 
