dd5391938052788d989e5940023c6fce0f66b108
sameena-waida <sameena_waida@nthdimenzion.com>
2015-03-20 16:20:20 +0530

    Adding Team UI

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,108 @@
+var AngularApp = {};
+var App = angular.module('AngularApp', ['ngRoute','ui.bootstrap','mgcrea.ngStrap.select','ngSanitize','angularFileUpload']);
+
+ App.controller('ViewTeamController',['$scope','$http',function($scope,$http){
+    $scope.teamList={};
+    $http.get('http://localhost:6443/pla/core/team/view').success(function(data){
+                   // console.log(data);
+           //$scope.teamList= data;
+    });
+    $scope.openAssignTeam = function(teamId){
+   var id=parseInt(teamId);
+        $http.get('http://localhost:6443/pla/core/team/openAssignPage/'+id).success(function(data){
+                          //   console.log(data);
+                   $scope.teamList= data;
+            });
+
+    }
+    $scope.openCreateTeam = function(){
+       // var id=parseInt(teamId);
+            $http.get('http://localhost:6443/pla/core/team/openCreatePage').success(function(data){
+                              //   console.log(data);
+                      // $scope.teamList= data;
+                });
+
+        }
+
+ }]);
+ App.controller('AssignTeamController',['$scope','$http',function($scope,$http){
+     $scope.newDateField={};
+     $scope.datePickerSettings = {
+         isOpened:false,
+          dateOptions:{
+              formatYear:'yyyy' ,
+              startingDay:1
+          }
+     }
+     $scope.open = function($event) {
+          $event.preventDefault();
+          $event.stopPropagation();
+          $scope.datePickerSettings.isOpened = true;
+     };
+     $scope.submitAssignTeam = function(){
+         if (!moment($scope.assignTeam.fromDate,'DD/MM/YYYY').isValid()) {
+         	    $scope.newDateField.fromDate = moment($scope.assignTeam.fromDate).format("DD/MM/YYYY");
+            	$scope.assignTeam.fromDate=$scope.newDateField.fromDate ;
+         }
+         console.log($scope.assignTeam);
+         $http.post('http://localhost:6443/pla/core/team/assign', $scope.assignTeam).success(function(data){
+                 console.log(data);
+
+         });
+     }
+
+  }]);
+
+
+
+ App.controller('CreateTeamController',['$scope','$http',function($scope,$http){
+     //   console.log("called CreateTeamController................");
+       	$scope.newDateField={};
+        $scope.datePickerSettings = {
+                isOpened:false,
+                 dateOptions:{
+                    formatYear:'yyyy' ,
+                    startingDay:1
+            }
+        }
+         $scope.open = function($event) {
+                  $event.preventDefault();
+                  $event.stopPropagation();
+                  $scope.datePickerSettings.isOpened = true;
+         };
+
+         $scope.getBranchList = function(regionName){
+                console.log(regionName);
+          		//$scope.branchList =_.findWhere($scope.employeeDetails,{regionName:regionName});
+        }
+         $scope.submitTeam = function(){
+
+             if (!moment($scope.createTeam.fromDate,'DD/MM/YYYY').isValid()) {
+             			$scope.newDateField.fromDate = moment($scope.createTeam.fromDate).format("DD/MM/YYYY");
+             			$scope.createTeam.fromDate=$scope.newDateField.fromDate ;
+
+             }
+             $scope.testTeamData={'teamName':'team A','teamCode':'123','regionCode':'555','branchCode':'666','employeeId':'100','fromDate':'02/05/2015','firstName':'Sara','lastName':'ali'}
+
+             $http.post('http://localhost:6443/pla/core/team/create', $scope.testTeamData).success(function(data){
+                                     console.log(data);
+
+
+             });
+            // console.log($scope.createTeam);
+
+             $scope.reset();
+
+         };
+         $scope.reset = function(){
+
+             $scope.branchName ='';
+             $scope.fromDate ='';
+         	 $scope.regionName ='';
+         	 $scope.teamCode ='';
+         	 $scope.teamLeader ='';
+         	 $scope.teamName ='';
+         	 }
+
+
+ }]);
--- src/main/resources/static/js/viewTeam.js
+++ src/main/resources/static/js/viewTeam.js
@@ -1,37 +1,5 @@
-require(['jquery','bootstrap','datatables','bootstrap-datepicker'],function(){
+require(['jquery','bootstrap','datatables'],function(){
 
     $('#team-table').dataTable();
 
-    $('#datetimepicker2').datepicker({
-            format: "dd/mm/yyyy",
-            orientation: "top auto",
-            clearBtn: true,
-            autoclose: true
-        });
-
 });
-var teamData = {
-     };
-var createTeamOne = function(){
-   $('#createTeam *').filter(':text').each(function(key,value){
-           teamData[$(value)[0].id]=$(value).val();
-
-       });
-        teamData["regionName"]= $('#regionName :selected').text();
-        teamData["branchName"]= $('#branchName :selected').text();
-        teamData["teamLeader"]= $('#teamLeader :selected').text();
-             console.log(teamData);
-             $.ajax({
-                url: '/pla/core/team/create',
-                type: 'POST',
-                data: JSON.stringify(teamData),
-                contentType: 'application/json; charset=utf-8',
-                success: function(msg) {
-                       if(msg=='success'){
-                            $('#alert').text("Team Created successfully").show();
-                        }
-                }
-             });
-
-
-    };
