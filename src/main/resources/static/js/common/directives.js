5e3260fdd831fda0ebe0c11c6de2bbd8c3729c1e
shivaraj-kv <shivaraj_vishwanathan@nthdimenzion.com>
2015-03-24 18:55:51 +0530

    Updates in wizard directive
    Parent scope value will be updated on change in the steps

--- src/main/resources/static/js/common/directives.js
+++ src/main/resources/static/js/common/directives.js
@@ -1,15 +1,14 @@
 define(['angular'],function(angular){
     angular.module('directives',[])
-        .directive('fueluxWizard',function(){
-            alert('fueluxWizard init invoked');
+        .directive('fueluxWizard',function($timeout){
             /*use this directive to initialize the fuelUx wizard
-            *
-            * example: <div fuelux-wizard selected-item="[step]">
-            * selected-item is used to initialize the current step the wizard is in.
+             *
+             * example: <div fuelux-wizard selected-item="[step]">
+             * selected-item is used to initialize the current step the wizard is in.
              * By changing the scope value of the [step], a watch triggers and is taken to the respective step.
-            */
+             */
             return{
-                restrict:'A',
+                restrict:'AEC',
                 scope:{
                     selectedItem:'='
                 },
@@ -17,6 +16,11 @@ define(['angular'],function(angular){
                     scope.$watch('selectedItem',function(newVal,oldVal){
                         element.wizard('selectedItem', {
                             step: newVal
+                        });
+                    });
+                    element.on('changed.fu.wizard',function(event,data){
+                        $timeout(function() {
+                            scope.selectedItem=data.step;
                         });
                     });
                     var wizardOptions={
