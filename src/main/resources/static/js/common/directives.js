f2eb29028f34697995de4a0f9c6c595c599dd542
shivaraj-kv <shivaraj_vishwanathan@nthdimenzion.com>
2015-03-25 11:26:41 +0530

    Updates in fuelux directive

--- src/main/resources/static/js/common/directives.js
+++ src/main/resources/static/js/common/directives.js
@@ -1,34 +1,32 @@
-define(['angular'],function(angular){
-    angular.module('directives',[])
-        .directive('fueluxWizard',function($timeout){
-            /*use this directive to initialize the fuelUx wizard
-             *
-             * example: <div fuelux-wizard selected-item="[step]">
-             * selected-item is used to initialize the current step the wizard is in.
-             * By changing the scope value of the [step], a watch triggers and is taken to the respective step.
-             */
-            return{
-                restrict:'AEC',
-                scope:{
-                    selectedItem:'='
-                },
-                link:function(scope,element,attr){
-                    scope.$watch('selectedItem',function(newVal,oldVal){
-                        element.wizard('selectedItem', {
-                            step: newVal
-                        });
+angular.module('directives',[])
+    .directive('fueluxWizard',function($timeout){
+        /*use this directive to initialize the fuelUx wizard
+         *
+         * example: <div fuelux-wizard selected-item="[step]">
+         * selected-item is used to initialize the current step the wizard is in.
+         * By changing the scope value of the [step], a watch triggers and is taken to the respective step.
+         */
+        return{
+            restrict:'AEC',
+            scope:{
+                selectedItem:'='
+            },
+            link:function(scope,element,attr){
+                scope.$watch('selectedItem',function(newVal,oldVal){
+                    element.wizard('selectedItem', {
+                        step: newVal
                     });
-                    element.on('changed.fu.wizard',function(event,data){
-                        $timeout(function() {
-                            scope.selectedItem=data.step;
-                        });
+                });
+                element.on('changed.fu.wizard',function(event,data){
+                    $timeout(function() {
+                        scope.selectedItem=data.step;
                     });
-                    var wizardOptions={
-                        disablePreviousStep:false,
-                        selectedItem:{step:scope.selectedItem}
-                    };
-                    element.wizard(wizardOptions);
-                }
+                });
+                var wizardOptions={
+                    disablePreviousStep:false,
+                    selectedItem:{step:scope.selectedItem}
+                };
+                element.wizard(wizardOptions);
             }
-        })
-});
+        }
+    })
