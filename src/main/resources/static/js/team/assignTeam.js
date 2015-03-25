ea4df43d628b72b3003fabdfeafcfd36eb1041e0
sameena-waida <sameena_waida@nthdimenzion.com>
2015-03-25 16:43:13 +0530

    Changes in Team UI

--- src/main/resources/static/js/team/assignTeam.js
+++ src/main/resources/static/js/team/assignTeam.js
@@ -1,6 +1,6 @@
 var App = angular.module('assignTeam', ['ngRoute','ui.bootstrap','mgcrea.ngStrap.select']);
 
-App.controller('AssignTeamController',['$scope','$http',function($scope,$http){
+App.controller('AssignTeamController',['$scope','$http','$window','$location',function($scope,$http,$window,$location){
 
      $scope.selectedDate =moment(new Date).format("YYYY-MM-DD");
      $scope.newDateField={};
@@ -16,14 +16,27 @@ App.controller('AssignTeamController',['$scope','$http',function($scope,$http){
           $event.stopPropagation();
           $scope.datePickerSettings.isOpened = true;
      };
-     $scope.submitAssignTeam = function(){
+     $http.get('http://localhost:6443/pla/core/team/getteamleaders').success(function(data){
+                   console.log(data);
+                   $scope.teamLeaders=data;
+     });
+      $scope.url = window.location.search.split('=')[1];
+      // console.log("WINDOW LOCATION-------"+$scope.url.split('=')[1]);
+
+         // var tid = $window.teamId;
+    // var id=parseInt(tid);
+     $http.get('http://localhost:6443/pla/core/team/openAssignPage?teamId='+$scope.url).success(function(data){
+                   console.log(data);
+                  // $scope.assignTeam=data;
+      });
+      $scope.submitAssignTeam = function(){
          if (!moment($scope.assignTeam.fromDate,'DD/MM/YYYY').isValid()) {
          	    $scope.newDateField.fromDate = moment($scope.assignTeam.fromDate).format("DD/MM/YYYY");
             	$scope.assignTeam.fromDate=$scope.newDateField.fromDate ;
          }
          console.log($scope.assignTeam);
          $http.post('http://localhost:6443/pla/core/team/assign', $scope.assignTeam).success(function(data){
-                 console.log(data);
+               console.log(data);
 
          });
      }
