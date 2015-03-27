cfd032b9e87aab234ba0ec0e8b47fb97ca9064a2
shivaraj-kv <shivaraj_vishwanathan@nthdimenzion.com>
2015-03-27 15:57:20 +0530

    Added common module
    Add this module as a dependency in all modules

---
+++ src/main/resources/static/js/common/commonModule.js
@@ -0,0 +1,3 @@
+define(['angular','angular-loading-bar','directives','angular-animate','ui-bootstrap-tpls'],function(angular){
+    angular.module('common',['directives','angular-loading-bar','ngAnimate','ui.bootstrap']);
+});
