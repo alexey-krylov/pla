bc588c863f28d65a886ea3f7717b7796285ce096
sameena-waida <sameena_waida@nthdimenzion.com>
2015-03-27 13:08:57 +0530

    Changes in Coverages UI

---
+++ src/main/resources/static/js/coverages/createCoverage.js
@@ -0,0 +1,35 @@
+var App = angular.module('createCoverage', ['ngRoute','ui.bootstrap','ngSanitize','mgcrea.ngStrap.select','mgcrea.ngStrap','mgcrea.ngStrap.alert']);
+
+App.controller('CreateCoverageController',['$scope','$http',function($scope,$http){
+          // console.log("CreateCoverageController called ***********************");
+          $http.get('http://localhost:6443/pla/core/benefit/activebenefits').success(function(data){
+                               //  console.log(data);
+                          $scope.getAllBenefits=data
+
+          });
+          /*$scope.getAllBenefits =[
+               {
+                 "benefitId": "3",
+                 "STATUS": "ACTIVE",
+                 "benefitName": "benefit four"
+               },
+               {
+                  "benefitId": "4",
+                  "STATUS": "ACTIVE",
+                  "benefitName": "benefit five"
+               },
+               {
+                   "benefitId": "5",
+                   "STATUS": "ACTIVE",
+                   "benefitName": "benefit two"
+               }]
+*/
+             /*$scope.submitCoverage = function(){
+                 console.log($scope.createCoverage);
+                 $http.post('http://localhost:6443/pla/core/coverages/create', $scope.createCoverage).success(function(data){
+                      console.log(data);
+
+                 });
+             }  */
+
+}]);
