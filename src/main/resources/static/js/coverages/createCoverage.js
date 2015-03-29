6e6fe2d1fe55170ea8b28601c19cb2d6273ce2c2
sameena-waida <sameena_waida@nthdimenzion.com>
2015-03-29 11:50:02 +0530

    Changes in Coverages UI

--- src/main/resources/static/js/coverages/createCoverage.js
+++ src/main/resources/static/js/coverages/createCoverage.js
@@ -1,35 +1,8 @@
 var App = angular.module('createCoverage', ['ngRoute','ui.bootstrap','ngSanitize','mgcrea.ngStrap.select','mgcrea.ngStrap','mgcrea.ngStrap.alert']);
 
-App.controller('CreateCoverageController',['$scope','$http',function($scope,$http){
-          // console.log("CreateCoverageController called ***********************");
+App.controller('CreateCoverageController',['$scope','$http','$rootScope',function($scope,$http,$rootScope){
+          $scope.createCoverage={};
           $http.get('http://localhost:6443/pla/core/benefit/activebenefits').success(function(data){
-                               //  console.log(data);
                           $scope.getAllBenefits=data
-
           });
-          /*$scope.getAllBenefits =[
-               {
-                 "benefitId": "3",
-                 "STATUS": "ACTIVE",
-                 "benefitName": "benefit four"
-               },
-               {
-                  "benefitId": "4",
-                  "STATUS": "ACTIVE",
-                  "benefitName": "benefit five"
-               },
-               {
-                   "benefitId": "5",
-                   "STATUS": "ACTIVE",
-                   "benefitName": "benefit two"
-               }]
-*/
-             /*$scope.submitCoverage = function(){
-                 console.log($scope.createCoverage);
-                 $http.post('http://localhost:6443/pla/core/coverages/create', $scope.createCoverage).success(function(data){
-                      console.log(data);
-
-                 });
-             }  */
-
 }]);
