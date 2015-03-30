b23967a1c2890cdf3680410b57991b53678241ed
Pradyumna Mohapatra <pradyumna.mohapatra@gmail.com>
2015-03-30 10:28:07 +0530

    Added Plan to Common Template

--- src/main/resources/static/js/common/directives.js
+++ src/main/resources/static/js/common/directives.js
@@ -1,46 +1,69 @@
-angular.module('directives',[])
-    .directive('nthTleafBinder',['$timeout',function($timeout){
-        return{
-            restrict:'A',
+angular.module('directives', [])
+    .directive('nthTleafBinder', ['$timeout', function ($timeout) {
+        return {
+            restrict: 'A',
             require: '?ngModel',
-            link:function(scope,element,attr,ngModel){
-                if(!ngModel) return;
+            link: function (scope, element, attr, ngModel) {
+                if (!ngModel) return;
                 var content = attr.nthTleafBinder;
-                ngModel.$render =function(){
+                ngModel.$render = function () {
                     element.val(content);
                     ngModel.$setViewValue(content);
                 };
             }
         }
     }])
-    .directive('fueluxWizard',function($timeout){
+    .directive('fueluxWizard', function ($timeout) {
         /*use this directive to initialize the fuelUx wizard
          *
          * example: <div fuelux-wizard selected-item="[step]">
          * selected-item is used to initialize the current step the wizard is in.
          * By changing the scope value of the [step], a watch triggers and is taken to the respective step.
          */
-        return{
-            restrict:'AEC',
-            scope:{
-                selectedItem:'='
+        return {
+            restrict: 'AEC',
+            require: ['^form'],
+            scope: {
+                selectedItem: '='
             },
-            link:function(scope,element,attr){
+            link: function (scope, element, attr, ctrl) {
+
                 scope.$watch('selectedItem',function(newVal,oldVal){
                     element.wizard('selectedItem', {
                         step: newVal
                     });
                 });
-                element.on('changed.fu.wizard',function(event,data){
-                    $timeout(function() {
-                        scope.selectedItem=data.step;
-                    });
+
+                $(element).wizard();
+                $(element).on('actionclicked.fu.wizard', function (event, data) {
+                    if (data.direction == 'previous')return;
+                    var stepForm = ctrl[0]['step' + data.step];
+                    validateStep(stepForm);
+                    console.log(stepForm);
+                    if (stepForm.$invalid) {
+                        event.preventDefault();
+                    }
+                    scope.$emit('actionclicked.fu.wizard', event, data);
                 });
-                var wizardOptions={
-                    disablePreviousStep:false,
-                    selectedItem:{step:scope.selectedItem}
-                };
-                element.wizard(wizardOptions);
+                $(element).on('stepclicked.fu.wizard', function (event, data) {
+                    scope.$emit('stepclicked.fu.wizard', event, data);
+                });
+
+                $(element).on('finished.fu.wizard', function (event) {
+                    scope.$emit('finished.fu.wizard', event);
+                });
+
+                this.validateStep = function (stepForm) {
+                    for (var propertyName in stepForm) {
+                        if (propertyName.charAt(0) != '$') {
+                            if (stepForm[propertyName].$invalid) {
+                                stepForm[propertyName].$setDirty(true);
+                            }
+                        }
+                    }
+                    scope.$apply();
+                }
+
             }
         }
     })
