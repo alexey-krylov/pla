f0e8f441f99dd41b2748c6a214043961b9053fc8
Samir Padhy <samir_padhy@nthdimenzion.com>
2015-03-31 12:24:10 +0530

    Benefit Selenium Test suit added

---
+++ src/test/java/com/pla/seleniumscripts/coverage/CoverageTestSuit.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 3/31/15 12:05 PM .NthDimenzion,Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+package com.pla.seleniumscripts.coverage;
+
+/**
+ * @author: Samir
+ * @since 1.0 31/03/2015
+ */
+public class CoverageTestSuit {
+}
