afa47797d911c9f143dca5d29aea67240c181b3a
shivaraj-kv <shivaraj_vishwanathan@nthdimenzion.com>
2015-04-06 16:16:17 +0530

    updates in common services

--- src/main/resources/static/js/common/services.js
+++ src/main/resources/static/js/common/services.js
@@ -1,8 +1,17 @@
 define(['moment'],function(moment){
     angular.module('commonServices',[])
-        .factory('formatJSDateToMilliseconds',function(){
+        .factory('formatJSDateToYYYYMMDD',function(){
             return function(date){
-               return moment(date).format('x');
+               return moment(date).format('YYYY-MM-DD');
             }
         })
+        .factory()
+        .factory('getQueryParameter',['$window',function($window){
+            return function(name){
+                name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+                var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+                    results = regex.exec($window.location.search);
+                return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, " "));
+            }
+        }])
 });
