efbe1338fa12502cc84f28652e717cfcd9c5e529
shivaraj-kv <shivaraj_vishwanathan@nthdimenzion.com>
2015-04-14 17:13:27 +0530

    Updates in commission module

--- src/main/resources/static/js/commissions/viewCommissions.js
+++ src/main/resources/static/js/commissions/viewCommissions.js
@@ -1,1 +1,16 @@
-$('#commission-table').dataTable();
+var viewCommission = (function(){
+    var commissionService = {};
+    var getCommissionMode = function(){
+        if(window.location.href.indexOf("NORMAL")!=-1){
+            return "NORMAL"
+        }
+        return "OVERRIDE"
+    };
+    commissionService.commissionMode = getCommissionMode();
+
+    commissionService.openCreatePage = function(){
+        window.location.href = "/pla/core/commission/opencreatepage?mode="+this.commissionMode;
+    };
+
+    return commissionService;
+})();
