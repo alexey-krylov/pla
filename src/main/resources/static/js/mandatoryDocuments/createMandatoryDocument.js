c795c104e34fe9591c9cf0fbb8fd864fd8b26cd0
sameena-waida <sameena_waida@nthdimenzion.com>
2015-04-14 15:18:41 +0530

    Changes in Premium UI

--- src/main/resources/static/js/mandatoryDocuments/createMandatoryDocument.js
+++ src/main/resources/static/js/mandatoryDocuments/createMandatoryDocument.js
@@ -1,15 +1,15 @@
 var App = angular.module('createMandatoryDocument', ['ngRoute','ui.bootstrap','ngSanitize','mgcrea.ngStrap.select','mgcrea.ngStrap','mgcrea.ngStrap.alert']);
 
-App.controller('CreateMandatoryDocumentController',['$scope','$http','$rootScope','$alert',function($scope,$http,$rootScope,$alert){
+App.controller('CreateMandatoryDocumentController',['$scope','$http','$rootScope','$alert','$window',function($scope,$http,$rootScope,$alert,$window){
 
           $scope.newPlanList = [];
-         $scope.showOptionalCoverage= false;
+          $scope.showOptionalCoverage= false;
+          $scope.boolVal=false;
           $http.get('/pla/core/master/getdocument').success(function(data){
-                          $scope.mandatoryDocList=data;
+               $scope.documentList=data;
+
           });
-          $http.get('/pla/core/mandatorydocument/getallmandatorydocument').success(function(data){
-                                    $scope.mandatoryDocList=data;
-                    });
+
          $http.get('/pla/core/plan/getallplan').success(function(data){
               for(var i=0;i<data.length;i++) {
                   $scope.planList=data[i];
@@ -23,8 +23,10 @@ App.controller('CreateMandatoryDocumentController',['$scope','$http','$rootScope
          $scope.getDefinedOption = function(){
              if($scope.createMandatoryDocument.definedFor == "plan"){
                    $scope.showOptionalCoverage= false;
+                   $scope.boolVal=true;
              }else{
                  $scope.showOptionalCoverage= true;
+                 $scope.boolVal=false;
              }
 
          }
@@ -34,11 +36,15 @@ App.controller('CreateMandatoryDocumentController',['$scope','$http','$rootScope
               var planId=$scope.createMandatoryDocument.planId;
               $scope.optionalCoverageData =_.findWhere($scope.newPlanList,{planId:planId});
               $scope.optionalCoverageList = _.where($scope.optionalCoverageData.coverages, {coverageType: "OPTIONAL"});
+
            }
          });
          $scope.$watch('createMandatoryDocument.coverageCode',function(newValue, oldValue){
            if(newValue){
                 $scope.createMandatoryDocument.coverageId=$scope.createMandatoryDocument.coverageCode.coverageId;
+               $scope.boolVal=true;
+           }else{
+               $scope.boolVal=false;
            }
          });
 
@@ -51,7 +57,8 @@ App.controller('CreateMandatoryDocumentController',['$scope','$http','$rootScope
             $http.post('/pla/core/mandatorydocument/create', $scope.createMandatoryDocument).success(function(data){
                   if(data.status==200){
                      $scope.alert = {title:'Success Message! ', content:data.message, type: 'success'};
-                     $scope.reset();
+                   //  $scope.reset();
+                     $window.location.reload();
                   }else{
                       $scope.alert = {title:'Error Message! ', content:data.message, type: 'danger'};
                   }
@@ -60,12 +67,11 @@ App.controller('CreateMandatoryDocumentController',['$scope','$http','$rootScope
          }
          $scope.reset = function(){
             $scope.createMandatoryDocument.definedFor='';
-             //$scope.showPlan=false;
-            //$scope.showOptionalCoverage=false;
             $scope.createMandatoryDocument.process='';
             $scope.createMandatoryDocument.documents='';
             $scope.createMandatoryDocument.planId='';
             $scope.createMandatoryDocument.coverageCode='';
+
          }
 
 
