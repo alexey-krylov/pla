facb111c6e8c24c616f3668118e24b01f0a14452
shivaraj-kv <shivaraj_vishwanathan@nthdimenzion.com>
2015-04-17 18:21:03 +0530

    Updates in common module

--- src/main/resources/static/js/common/commonModule.js
+++ src/main/resources/static/js/common/commonModule.js
@@ -1,15 +1,25 @@
 define(['angular','angular-loading-bar','directives','angular-animate','ui-bootstrap-tpls'],function(angular){
     angular.module('common',['directives','angular-loading-bar','ngAnimate','ui.bootstrap'])
         .constant('globalConstants',{
-            numberPattern : /^\d+$/
+            numberPattern : /^\d+$/,
+            title:["Mr.","Mrs.","Miss","Dr.","Prof.","Hon.","Ms.","Rev.","Pst."],
+            gender:[{code:'MALE',description:"Male"},{code:'FEMALE',description:'Female'}]
         })
         .factory('nthHttpInterceptor',['$rootScope',function($rootScope){
             return {
                 'response': function(response) {
                     if(response.config.method=="POST" && response.status==200){
-                        $rootScope.$broadcast('httpInterceptorAlert',{
-                            message:response.data.message
-                        });
+                        if(response.data.status=="500"){
+                            $rootScope.$broadcast('httpInterceptorAlert',{
+                                message:response.data.message,
+                                type:"danger"
+                            });
+                        }else{
+                            $rootScope.$broadcast('httpInterceptorAlert',{
+                                message:response.data.message,
+                                type:"success"
+                            });
+                        }
                     }
                     return response;
                 }
@@ -17,5 +27,5 @@ define(['angular','angular-loading-bar','directives','angular-animate','ui-boots
         }])
         .config(['$httpProvider',function($httpProvider){
             $httpProvider.interceptors.push('nthHttpInterceptor');
-        }])
+        }]);
 });
