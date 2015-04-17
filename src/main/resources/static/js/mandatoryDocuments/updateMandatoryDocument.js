cbca4aacdbf77f0fb08da7d25cfa7d49dafb8ffc
sameena-waida <sameena_waida@nthdimenzion.com>
2015-04-17 23:53:49 +0530

    Changes in Premium and Mandatory document UI

--- src/main/resources/static/js/mandatoryDocuments/updateMandatoryDocument.js
+++ src/main/resources/static/js/mandatoryDocuments/updateMandatoryDocument.js
@@ -3,7 +3,7 @@ var App = angular.module('updateMandatoryDocument', ['ngRoute','ui.bootstrap','n
 App.controller('UpdateMandatoryDocumentsController',['$scope','$http','$rootScope','$window','$location','$alert',function($scope,$http,$rootScope,$window,$location,$alert){
 
           $scope.showOptionalCoverage=false;
-
+          $scope.boolVal=false;
           $scope.url = window.location.search.split('=')[1];
           $http.get('/pla/core/master/getdocument').success(function(data){
             $scope.mandatoryDocList=data;
@@ -19,6 +19,18 @@ App.controller('UpdateMandatoryDocumentsController',['$scope','$http','$rootScop
                   }
 
           });
+    $scope.$watch('updateMandatoryDocument.document',function(newValue, oldValue){
+        if(newValue) {
+            if (newValue.length > 0) {
+
+                $scope.boolVal = true;
+            } else {
+                $scope.boolVal = false;
+            }
+        } else {
+            $scope.boolVal = false;
+        }
+    });
 
          $scope.updateMandatoryDoc = function(){
             //console.log($scope.updateMandatoryDocument);
@@ -26,6 +38,7 @@ App.controller('UpdateMandatoryDocumentsController',['$scope','$http','$rootScop
             $http.post('/pla/core/mandatorydocument/update',$scope.updateMandatoryDocument).success(function(data){
                   if(data.status==200){
                      $scope.alert = {title:'Success Message! ', content:data.message, type: 'success'};
+                      $window.location.href="/pla/core/mandatorydocument/view";
                    //  $scope.reset();
                   }else{
                       $scope.alert = {title:'Error Message! ', content:data.message, type: 'danger'};
