cc9dc3ad3e3aed6e3ccffc733b958bba4a3f8b25
sameena-waida <sameena_waida@nthdimenzion.com>
2015-04-23 13:32:28 +0530

    Changes in OrganizationLevelInformation UI

--- src/main/resources/static/js/generalInformation/updateOrganizationalLevelInformation.js
+++ src/main/resources/static/js/generalInformation/updateOrganizationalLevelInformation.js
@@ -4,42 +4,48 @@ App.controller('UpdateOrganizationalLevelController',['$scope','$http','$templat
     $scope.organizationList=[];
     $scope.organizationLevelInformation={};
     $scope.updateOrganizationinfo=false;
-    var createObject = function(){
-        var transformedObject = {};
-        angular.forEach($scope.organizationLevelInformation,function(value,key){
 
-            if(key!='serviceTax'){
-                transformedObject[key] =[value];
-            }else{
-                transformedObject[key] = value;
-            }
-
-        })
-        return transformedObject;
-    }
     $http.get('/pla/core/organizationinformation/getorganizationformation').success(function(data){
 
-       //$scope.organizationInformation=data[0];
-        $scope.organizationLevelInformation=data[0];
+       $scope.organizationInformation=data[0];
+
      //  console.log($scope.organizationLevelInformation);
-        if($scope.organizationLevelInformation.organizationInformationId!=null){
+        if($scope.organizationInformation.organizationInformationId!=null){
             $scope.updateOrganizationinfo=true;
         }else{
             $scope.updateOrganizationinfo=false;
         }
 
     });
+    $http.get('/pla/core/organizationinformation/getorganizationprocessitem').success(function(data){
+        $scope.processItems=data;
+
+    });
+
+    $scope.fieldData={};
+    $scope.getFieldName = function(fieldtype){
+        //  console.log(fieldtype);
+        if(fieldtype){
+            $scope.fieldData=_.findWhere($scope.processItems,{type:fieldtype});
+            if($scope.fieldData)
+                return $scope.fieldData.description;
+        }
+    }
+    $scope.reloadPage = function(){
+        $window.location.href="/pla/core/organizationinformation/openview";
+
+    }
 
     $scope.submitOrganizationLevelInfo = function(){
-        //console.log(createObject());
-   //console.log( $scope.organizationLevelInformation);
-           $http.post('/pla/core/organizationinformation/update',  $scope.organizationLevelInformation).success(function (data) {
-                console.log(data.status);
+
+           $http.post('/pla/core/organizationinformation/update',  $scope.organizationInformation).success(function (data) {
+
                 if (data.status == 200) {
+
                     $scope.alert = {title: 'Success Message! ', content: data.message, type: 'success'};
                     // $scope.reset();
                    // $window.location.reload();
-                    $window.location.href="/pla/core/organizationinformation/openview";
+                   // $window.location.href="/pla/core/organizationinformation/openview";
 
                 } else if (data.status == 500) {
                     $scope.alert = {title: 'Error Message! ', content: data.message, type: 'danger'};
@@ -64,42 +70,40 @@ App.controller('CreateOrganizationalLevelController',['$scope','$http','$templat
     $scope.createOrganizationinfo=false;
     $http.get('/pla/core/organizationinformation/getorganizationformation').success(function(data){
         $scope.organizationInformation=data[0];
+
         if($scope.organizationInformation.organizationInformationId==null){
             $scope.createOrganizationinfo=true;
         }else{
             $scope.createOrganizationinfo=false;
         }
     });
-    var createObject = function(){
-        var transformedObject = {};
-        angular.forEach($scope.createOrganizationLevelInformation,function(value,key){
+    $http.get('/pla/core/organizationinformation/getorganizationprocessitem').success(function(data){
+        $scope.processItems=data;
 
-            if(key=='modelFactorItems'){
-                transformedObject[key]=[{"SEMI_ANNUAL":$scope.createOrganizationLevelInformation.modelFactorItems.SEMI_ANNUAL},{"QUARTERLY" :$scope.createOrganizationLevelInformation.modelFactorItems.QUARTERLY},{"MONTHLY":$scope.createOrganizationLevelInformation.modelFactorItems.MONTHLY}];
-            }else if(key=='discountFactorItems'){
-                transformedObject[key]=[{"SEMI_ANNUAL":$scope.createOrganizationLevelInformation.discountFactorItems.SEMI_ANNUAL},{"ANNUAL":$scope.createOrganizationLevelInformation.discountFactorItems.ANNUAL},{"QUARTERLY":$scope.createOrganizationLevelInformation.discountFactorItems.QUARTERLY}];
+    });
 
-            }else{
-               transformedObject[key] = value;
-            }
-        })
-
-        return transformedObject;
+    $scope.fieldData={};
+    $scope.getFieldName = function(fieldtype){
+        //  console.log(fieldtype);
+        if(fieldtype){
+            $scope.fieldData=_.findWhere($scope.processItems,{type:fieldtype});
+            if($scope.fieldData)
+                return $scope.fieldData.description;
+        }
+    }
+    $scope.reloadPage = function(){
+        $window.location.href="/pla/core/organizationinformation/openview";
 
     }
 
-
-
     $scope.createOrganizationLevelInfo = function() {
-    //console.log(createObject());
-
-       $http.post('/pla/core/organizationinformation/create', createObject()).success(function (data) {
+       $http.post('/pla/core/organizationinformation/create', $scope.organizationInformation).success(function (data) {
 
             if (data.status == 200) {
                 $scope.alert = {title: 'Success Message! ', content: data.message, type: 'success'};
                 // $scope.reset();
                // $window.location.reload();
-               // $window.location.href="/pla/core/organizationinformation/openview";
+             //  $window.location.href="/pla/core/organizationinformation/openview";
 
             } else if (data.status == 500) {
                 $scope.alert = {title: 'Error Message! ', content: data.message, type: 'danger'};
