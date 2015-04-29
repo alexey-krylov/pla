09d57f6c245a24fc2bff3db9561f41a17e59f767
Pradyumna Mohapatra <pradyumna.mohapatra@gmail.com>
2015-04-29 15:32:49 +0530

    Bug Fixes 10757,10762,10778,10779,10781,10790,10794,10796,10798,

--- src/main/java/com/pla/sharedkernel/domain/model/PlanStatus.java
+++ src/main/java/com/pla/sharedkernel/domain/model/PlanStatus.java
@@ -3,7 +3,7 @@ package com.pla.sharedkernel.domain.model;
 /**
  * Created by pradyumna on 23-04-2015.
  */
-public enum PlanStatusEnum {
+public enum PlanStatus {
 
     DRAFT, PREMIUM_CONFIGURED, LAUNCHED, WITHDRAWN
 }
