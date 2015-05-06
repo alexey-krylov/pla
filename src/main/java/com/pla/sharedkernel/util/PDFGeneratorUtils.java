c38f6425ebfa9987b27919fab9db17655b95332d
nischitharamanna12 <nischitha_ramanna@nthdimenzion.com>
2015-05-06 11:57:35 +0530

    GL quotation removed unused statements

--- src/main/java/com/pla/sharedkernel/util/PDFGeneratorUtils.java
+++ src/main/java/com/pla/sharedkernel/util/PDFGeneratorUtils.java
@@ -3,9 +3,7 @@ package com.pla.sharedkernel.util;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 
@@ -18,7 +16,6 @@ public class PDFGeneratorUtils {
     public static <T> byte[] createPDFReportByList(List<T> reportData, String jasperFileName) throws IOException, JRException {
         JasperReport jasperReport = JasperCompileManager.compileReport(PDFGeneratorUtils.class.getClassLoader().getResourceAsStream("jasperpdf/template/grouplife/glQuotation.jrxml"));
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, new HashMap(), new JRBeanCollectionDataSource(reportData, true));
-        OutputStream outputStream = new ByteArrayOutputStream();
         return JasperExportManager.exportReportToPdf(jasperPrint);
     }
 }
