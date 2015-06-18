b6c7ac414de0714c415f21727a02c7a715feb5f2
nischitharamanna12 <nischitha_ramanna@nthdimenzion.com>
2015-06-18 18:35:31 +0530

    GL Proposal

--- src/main/resources/static/js/proposal/groupLife/viewProposal.js
+++ src/main/resources/static/js/proposal/groupLife/viewProposal.js
@@ -1,49 +1,48 @@
-var viewProposalModule = (function(){
+var viewProposalModule = (function () {
     var services = {};
     services.selectedItem = "";
-    services.getTheItemSelected = function(ele){
-        this.selectedItem=$(ele).val();
-        $(".btn-disabled").attr("disabled",false);
+    services.getTheItemSelected = function (ele) {
+        this.selectedItem = $(ele).val();
+        $(".btn-disabled").attr("disabled", false);
     };
 
-
-    services.reload = function(){
+    services.reload = function () {
         window.location.reload();
     };
 
-    services.createProposal = function(){
-        window.location.href = "creategrouplifeproposal"
+    services.createQuotation = function () {
+        window.location.href = "creategrouplifequotation"
     };
 
-    services.modifyProposal = function(){
-        var proposalId = this.selectedItem;
+    services.modifyQuotation = function () {
+        var quotationId = this.selectedItem;
         $.ajax({
-            url: '/pla/proposal/grouplife/getversionnumber/'+proposalId,
+            url: '/pla/proposal/grouplife/getversionnumber/' + quotationId,
             type: 'GET',
             contentType: 'application/json; charset=utf-8',
-            success: function(msg) {
-                if(msg.status=='200'){
-                    window.location.href = "/pla/proposal/grouplife/creategrouplifeproposal?proposalId="+proposalId+"&version="+msg.data+"&mode=edit";
-                }else if(msg.status=='500'){
+            success: function (msg) {
+                if (msg.status == '200') {
+                    window.location.href = "/pla/proposal/grouplife/creategrouplifeproposal?quotationId=" + quotationId + "&version=" + msg.data + "&mode=edit";
+                } else if (msg.status == '500') {
                 }
             }
         });
     };
 
-    services.viewProposal = function(){
-        var proposalId = this.selectedItem;
+    services.viewQuotation = function () {
+        var quotationId = this.selectedItem;
         $.ajax({
-            url: '/pla/proposal/grouplife/getversionnumber/'+proposalId,
+            url: '/pla/quotation/grouplife/getversionnumber/' + quotationId,
             type: 'GET',
             contentType: 'application/json; charset=utf-8',
-            success: function(msg) {
-                if(msg.status=='200'){
-                    window.location.href = "/pla/proposal/grouplife/creategrouplifeproposal?proposalId="+proposalId+"&version="+msg.data+"&mode=view";
-                }else if(msg.status=='500'){
+            success: function (msg) {
+                if (msg.status == '200') {
+                    window.location.href = "/pla/quotation/grouplife/creategrouplifequotation?quotationId=" + quotationId + "&version=" + msg.data + "&mode=view";
+                } else if (msg.status == '500') {
                 }
             }
         });
     };
 
-    return services ;
+    return services;
 })();
