58957e239bd981143690784e7b3cf58bba644f39
samir-padhy <samir_padhy@nthdimenzion.com>
2015-07-01 13:11:18 +0530

    Premium can be zero change

--- src/main/java/com/pla/grouphealth/sharedresource/model/vo/GHFrequencyPremium.java
+++ src/main/java/com/pla/grouphealth/sharedresource/model/vo/GHFrequencyPremium.java
@@ -9,8 +9,6 @@ import org.nthdimenzion.ddd.domain.annotations.ValueObject;
 
 import java.math.BigDecimal;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
 /**
  * Created by Samir on 4/7/2015.
  */
@@ -26,8 +24,6 @@ public class GHFrequencyPremium {
     private BigDecimal premium;
 
     public GHFrequencyPremium(PremiumFrequency premiumFrequency, BigDecimal premium) {
-        checkArgument(premiumFrequency != null);
-        checkArgument(premium != null && premium.compareTo(BigDecimal.ZERO) == 1);
         this.premiumFrequency = premiumFrequency;
         this.premium = premium;
     }
