f8eb4fc1104e0e69a19dde2e3d759ed8106a0617
samir-padhy <samir_padhy@nthdimenzion.com>
2015-07-07 17:41:04 +0530

    Mandatory document fix

--- src/main/java/com/pla/underwriter/finder/UnderWriterFinder.java
+++ src/main/java/com/pla/underwriter/finder/UnderWriterFinder.java
@@ -52,7 +52,7 @@ public class UnderWriterFinder {
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Autowired
-    public UnderWriterFinder(UnderWriterDocumentRepository underWriterDocumentRepository,UnderWriterRoutingLevelRepository underWriterRoutingLevelRepository) {
+    public UnderWriterFinder(UnderWriterDocumentRepository underWriterDocumentRepository, UnderWriterRoutingLevelRepository underWriterRoutingLevelRepository) {
         objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JodaModule());
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
@@ -62,7 +62,7 @@ public class UnderWriterFinder {
                 .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
                 .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
                 .withCreatorVisibility(JsonAutoDetect.Visibility.ANY));
-        this.underWriterDocumentRepository  = underWriterDocumentRepository;
+        this.underWriterDocumentRepository = underWriterDocumentRepository;
         this.underWriterRoutingLevelRepository = underWriterRoutingLevelRepository;
     }
 
@@ -72,14 +72,9 @@ public class UnderWriterFinder {
         this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
     }
 
-    public static final String FIND_ALL_DOCUMENT_APPROVED_BY_SERVICE_PROVIDER =  "SELECT documentName,documentCode FROM document_view WHERE isProvided = 'YES'";
+    public static final String FIND_ALL_DOCUMENT_APPROVED_BY_SERVICE_PROVIDER = "SELECT documentName,documentCode FROM document_view WHERE isProvided = 'YES'";
 
-    public static  String findMandatoryDocumentByProcessType = "SELECT ms.document_code documentCode,d.document_name documentName FROM mandatory_document md INNER JOIN mandatory_documents ms   " +
-            "   ON md.document_id = ms.document_id  " +
-            "   INNER JOIN document d ON ms.document_code = d.document_code " +
-            "   WHERE md.process = :processType  AND md.plan_id=:planId ";
-
-    public static final String FIND_DOCUMENT_DETAIL_BY_DOCUMENT_CODE =  "SELECT documentName,documentCode FROM document_view WHERE documentCode in(:documentIds)";
+    public static final String FIND_DOCUMENT_DETAIL_BY_DOCUMENT_CODE = "SELECT documentName,documentCode FROM document_view WHERE documentCode in(:documentIds)";
 
     public static final String FIND_PLAN_COVERAGE_DETAIL_BY_PLAN_CODE = "SELECT DISTINCT planName,c.coverage_name coverageName FROM  plan_coverage_benefit_assoc_view p INNER JOIN coverage c " +
             "   ON coverageId = c.coverage_id " +
@@ -88,15 +83,15 @@ public class UnderWriterFinder {
     public static final String FIND_PLAN_NAME_BY_CODE = "SELECT planName FROM plan_coverage_benefit_assoc_view WHERE planCode =:code LIMIT 1";
 
     public List<Map> findAllUnderWriterDocument() {
-        List<UnderWriterDocument> allUnderWriterDocument =  underWriterDocumentRepository.findEffectiveUnderWriterDocument();
+        List<UnderWriterDocument> allUnderWriterDocument = underWriterDocumentRepository.findEffectiveUnderWriterDocument();
         List<Map> underWriterDocumentList = new ArrayList<Map>();
         for (UnderWriterDocument underWriterDocument : allUnderWriterDocument) {
-            Map<String,Object> underWriterDocumentMap = objectMapper.convertValue(underWriterDocument, Map.class);
+            Map<String, Object> underWriterDocumentMap = objectMapper.convertValue(underWriterDocument, Map.class);
             underWriterDocumentMap.put("processType", UnderWriterProcessType.valueOf((String) underWriterDocumentMap.get("processType")).getDescription());
-            String coverageId = underWriterDocument.getCoverageId()!=null?underWriterDocument.getCoverageId().getCoverageId():null;
-            underWriterDocumentMap = planCoverageDetailTransformer(coverageId,underWriterDocument.getPlanCode(),underWriterDocumentMap);
-            underWriterDocumentMap.put("effectiveFrom", underWriterDocumentMap.get("effectiveFrom")!=null? DateTime.parse(underWriterDocumentMap.get("effectiveFrom").toString()).toString(AppConstants.DD_MM_YYY_FORMAT):null);
-            underWriterDocumentMap.put("validTill", underWriterDocumentMap.get("validTill")!=null?DateTime.parse(underWriterDocumentMap.get("validTill").toString()).toString(AppConstants.DD_MM_YYY_FORMAT):null);
+            String coverageId = underWriterDocument.getCoverageId() != null ? underWriterDocument.getCoverageId().getCoverageId() : null;
+            underWriterDocumentMap = planCoverageDetailTransformer(coverageId, underWriterDocument.getPlanCode(), underWriterDocumentMap);
+            underWriterDocumentMap.put("effectiveFrom", underWriterDocumentMap.get("effectiveFrom") != null ? DateTime.parse(underWriterDocumentMap.get("effectiveFrom").toString()).toString(AppConstants.DD_MM_YYY_FORMAT) : null);
+            underWriterDocumentMap.put("validTill", underWriterDocumentMap.get("validTill") != null ? DateTime.parse(underWriterDocumentMap.get("validTill").toString()).toString(AppConstants.DD_MM_YYY_FORMAT) : null);
             underWriterDocumentList.add(underWriterDocumentMap);
         }
         return underWriterDocumentList;
@@ -108,24 +103,24 @@ public class UnderWriterFinder {
         for (UnderWriterRoutingLevel underWriterRoutingLevel : allUnderWriterRoutingLevel) {
             Map underWritingRoutingLevelMap = objectMapper.convertValue(underWriterRoutingLevel, Map.class);
             underWritingRoutingLevelMap = underWriterInfluencingFactorAndProcessTypeTransformer(underWritingRoutingLevelMap);
-            String coverageId = underWriterRoutingLevel.getCoverageId()!=null?underWriterRoutingLevel.getCoverageId().getCoverageId():null;
-            underWritingRoutingLevelMap = planCoverageDetailTransformer(coverageId,underWriterRoutingLevel.getPlanCode(),underWritingRoutingLevelMap);
-            underWritingRoutingLevelMap.put("effectiveFrom", underWritingRoutingLevelMap.get("effectiveFrom")!=null? DateTime.parse(underWritingRoutingLevelMap.get("effectiveFrom").toString()).toString(AppConstants.DD_MM_YYY_FORMAT):null);
+            String coverageId = underWriterRoutingLevel.getCoverageId() != null ? underWriterRoutingLevel.getCoverageId().getCoverageId() : null;
+            underWritingRoutingLevelMap = planCoverageDetailTransformer(coverageId, underWriterRoutingLevel.getPlanCode(), underWritingRoutingLevelMap);
+            underWritingRoutingLevelMap.put("effectiveFrom", underWritingRoutingLevelMap.get("effectiveFrom") != null ? DateTime.parse(underWritingRoutingLevelMap.get("effectiveFrom").toString()).toString(AppConstants.DD_MM_YYY_FORMAT) : null);
             underWritingRoutingLevelMap.put("validTill", underWritingRoutingLevelMap.get("validTill") != null ? DateTime.parse(underWritingRoutingLevelMap.get("validTill").toString()).toString(AppConstants.DD_MM_YYY_FORMAT) : null);
             underWritingRoutingLevelList.add(underWritingRoutingLevelMap);
         }
         return underWritingRoutingLevelList;
     }
 
-    private Map underWriterInfluencingFactorAndProcessTypeTransformer(Map underWritingRoutingLevelMap){
+    private Map underWriterInfluencingFactorAndProcessTypeTransformer(Map underWritingRoutingLevelMap) {
         List underWriterRoutingLevelInfluencingFactor = (List) underWritingRoutingLevelMap.get("underWriterInfluencingFactors");
         underWritingRoutingLevelMap.put("underWriterInfluencingFactors", underWriterRoutingLevelInfluencingFactor.parallelStream().map(new Function<Object, String>() {
             @Override
             public String apply(Object underWriterInfluencingFactor) {
-                return UnderWriterInfluencingFactor.valueOf((String)underWriterInfluencingFactor).getDescription();
+                return UnderWriterInfluencingFactor.valueOf((String) underWriterInfluencingFactor).getDescription();
             }
         }).collect(Collectors.toList()));
-        underWritingRoutingLevelMap.put("processType", UnderWriterProcessType.valueOf((String)underWritingRoutingLevelMap.get("processType")).getDescription());
+        underWritingRoutingLevelMap.put("processType", UnderWriterProcessType.valueOf((String) underWritingRoutingLevelMap.get("processType")).getDescription());
         return underWritingRoutingLevelMap;
     }
 
@@ -141,18 +136,18 @@ public class UnderWriterFinder {
         return namedParameterJdbcTemplate.query(FIND_ALL_DOCUMENT_APPROVED_BY_SERVICE_PROVIDER, new ColumnMapRowMapper());
     }
 
-    public UnderWriterDocument getUnderWriterDocumentSetUp(String planCode,CoverageId coverageId,LocalDate effectiveFrom,String processType){
-        List<UnderWriterDocument> underWriterDocument =  underWriterDocumentRepository.findUnderWriterDocument(planCode, coverageId, effectiveFrom, null, processType);
+    public UnderWriterDocument getUnderWriterDocumentSetUp(String planCode, CoverageId coverageId, LocalDate effectiveFrom, String processType) {
+        List<UnderWriterDocument> underWriterDocument = underWriterDocumentRepository.findUnderWriterDocument(planCode, coverageId, effectiveFrom, null, processType);
         checkArgument(isNotEmpty(underWriterDocument), "Under Writer Document can not be null");
         checkArgument(underWriterDocument.size() == 1);
         return underWriterDocument.get(0);
     }
 
-    public Map<String,Object> getUnderWriterDocumentById(String underWriterRoutingLevelId){
+    public Map<String, Object> getUnderWriterDocumentById(String underWriterRoutingLevelId) {
         UnderWriterDocument underWriterDocument = underWriterDocumentRepository.findOne(new UnderWriterDocumentId(underWriterRoutingLevelId));
-        Map<String,Object> underWriterDocumentMap = objectMapper.convertValue(underWriterDocument, Map.class);
-        List<Map<String,Object>> underWriterLineItemList = underWriterDocument.transformUnderWriterDocumentLineItem();
-        underWriterLineItemList = underWriterLineItemList.stream().map(new Function<Map<String,Object>, Map<String,Object>>() {
+        Map<String, Object> underWriterDocumentMap = objectMapper.convertValue(underWriterDocument, Map.class);
+        List<Map<String, Object>> underWriterLineItemList = underWriterDocument.transformUnderWriterDocumentLineItem();
+        underWriterLineItemList = underWriterLineItemList.stream().map(new Function<Map<String, Object>, Map<String, Object>>() {
             @Override
             public Map<String, Object> apply(Map<String, Object> underWriterDocumentLineItem) {
                 Set<String> documentIds = (Set<String>) underWriterDocumentLineItem.get("underWriterDocuments");
@@ -160,17 +155,17 @@ public class UnderWriterFinder {
                 return underWriterDocumentLineItem;
             }
         }).collect(Collectors.toList());
-        String coverageId =  underWriterDocument.getCoverageId()!=null?underWriterDocument.getCoverageId().getCoverageId():null;
-        underWriterDocumentMap = planCoverageDetailTransformer(coverageId,underWriterDocument.getPlanCode(),underWriterDocumentMap);
+        String coverageId = underWriterDocument.getCoverageId() != null ? underWriterDocument.getCoverageId().getCoverageId() : null;
+        underWriterDocumentMap = planCoverageDetailTransformer(coverageId, underWriterDocument.getPlanCode(), underWriterDocumentMap);
         underWriterDocumentMap.put("underWriterDocumentItems", underWriterLineItemList);
         return underWriterDocumentMap;
     }
 
-    private List<Map<String,Object>> getDocumentDetailByDocumentCode(List<String> listOfDocumentCode){
-        return namedParameterJdbcTemplate.query(FIND_DOCUMENT_DETAIL_BY_DOCUMENT_CODE,new MapSqlParameterSource("documentIds", listOfDocumentCode),new ColumnMapRowMapper());
+    private List<Map<String, Object>> getDocumentDetailByDocumentCode(List<String> listOfDocumentCode) {
+        return namedParameterJdbcTemplate.query(FIND_DOCUMENT_DETAIL_BY_DOCUMENT_CODE, new MapSqlParameterSource("documentIds", listOfDocumentCode), new ColumnMapRowMapper());
     }
 
-    private Map<String,Object> planCoverageDetailTransformer(String coverageId,String planCode,Map<String,Object> underWriterMap){
+    private Map<String, Object> planCoverageDetailTransformer(String coverageId, String planCode, Map<String, Object> underWriterMap) {
         underWriterMap.put("planName", "");
         underWriterMap.put("coverageName", "");
         if (isNotEmpty(coverageId)) {
@@ -180,18 +175,30 @@ public class UnderWriterFinder {
                 underWriterMap.put("planName", planCoverageDetail.get(0).get("planName"));
                 underWriterMap.put("coverageName", planCoverageDetail.get(0).get("coverageName"));
             }
-        }
-        else {
-            SqlParameterSource sqlParameterSource = new MapSqlParameterSource("code",planCode);
-            String planName =(String) namedParameterJdbcTemplate.queryForMap(FIND_PLAN_NAME_BY_CODE, sqlParameterSource).get("planName");
-            underWriterMap.put("planName",planName);
+        } else {
+            SqlParameterSource sqlParameterSource = new MapSqlParameterSource("code", planCode);
+            String planName = (String) namedParameterJdbcTemplate.queryForMap(FIND_PLAN_NAME_BY_CODE, sqlParameterSource).get("planName");
+            underWriterMap.put("planName", planName);
         }
         return underWriterMap;
     }
 
-    public List<Map<String,Object>> getMandatoryDocumentForPlanAndCoverage(String planId, List<CoverageId> coverageIds, ProcessType processType) {
-        SqlParameterSource sqlParameterSource = new MapSqlParameterSource("processType",processType.name()).addValue("planId",planId).addValue("coverageIds",coverageIds);
-        findMandatoryDocumentByProcessType = isEmpty(coverageIds)?findMandatoryDocumentByProcessType+" AND md.coverage_id is null ":findMandatoryDocumentByProcessType+" AND md.coverage_id in (:coverageIds)";
+    public List<Map<String, Object>> getMandatoryDocumentForPlanAndCoverage(String planId, List<CoverageId> coverageIds, ProcessType processType) {
+        String findMandatoryDocumentByProcessType = "SELECT ms.document_code documentCode,d.document_name documentName FROM mandatory_document md INNER JOIN mandatory_documents ms   " +
+                "   ON md.document_id = ms.document_id  " +
+                "   INNER JOIN document d ON ms.document_code = d.document_code " +
+                "   WHERE md.process = :processType  AND md.plan_id=:planId ";
+        List<String> coverageIdsInString = null;
+        if (isNotEmpty(coverageIds)) {
+            coverageIdsInString = coverageIds.stream().map(new Function<CoverageId, String>() {
+                @Override
+                public String apply(CoverageId coverageId) {
+                    return coverageId.getCoverageId();
+                }
+            }).collect(Collectors.toList());
+        }
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource("processType", processType.name()).addValue("planId", planId).addValue("coverageIds", coverageIds);
+        findMandatoryDocumentByProcessType = isEmpty(coverageIdsInString) ? findMandatoryDocumentByProcessType + " AND md.coverage_id is null " : findMandatoryDocumentByProcessType + " AND md.coverage_id in (:coverageIds)";
         return namedParameterJdbcTemplate.query(findMandatoryDocumentByProcessType, sqlParameterSource, new ColumnMapRowMapper());
     }
 }
