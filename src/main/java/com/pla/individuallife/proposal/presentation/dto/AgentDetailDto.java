1fd0a53d8759e1be1fe85bb42aa02cd6d7e301bb
ykarunakar <karunakar_y@nthdimenzion.com>
2015-07-09 12:18:54 +0530

    IL Proposal changes to add agnetfirstname

--- src/main/java/com/pla/individuallife/proposal/presentation/dto/AgentDetailDto.java
+++ src/main/java/com/pla/individuallife/proposal/presentation/dto/AgentDetailDto.java
@@ -16,6 +16,6 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class AgentDetailDto {
     private String agentId;
-    private String agentName;
+    private String firstName;
     private BigDecimal commission;
 }
