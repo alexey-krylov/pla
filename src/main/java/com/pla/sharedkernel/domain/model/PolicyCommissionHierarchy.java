3ce02e1066896ee5ffb7af7856d8704be4476b4f
samir-padhy <samir_padhy@nthdimenzion.com>
2015-07-13 13:19:59 +0530

    GL quotation PDF issue fix

--- src/main/java/com/pla/sharedkernel/domain/model/PolicyCommissionHierarchy.java
+++ src/main/java/com/pla/sharedkernel/domain/model/PolicyCommissionHierarchy.java
@@ -1,8 +1,14 @@
 package com.pla.sharedkernel.domain.model;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by Samir on 7/9/2015.
  */
+@Getter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class PolicyCommissionHierarchy {
 
     private Agent agent;
@@ -14,4 +20,12 @@ public class PolicyCommissionHierarchy {
     private BranchBDE branchBDE;
 
     private RegionalManager regionalManager;
+
+    public PolicyCommissionHierarchy(Agent agent, TeamLeader teamLeader, BranchManager branchManager, BranchBDE branchBDE, RegionalManager regionalManager) {
+        this.agent = agent;
+        this.teamLeader = teamLeader;
+        this.branchManager = branchManager;
+        this.branchBDE = branchBDE;
+        this.regionalManager = regionalManager;
+    }
 }
