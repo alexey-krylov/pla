9ef7c6d5cb4de3b3e06b3d398d70922c97066e14
prasant01 <prasant_nayak@nthdimenzion.com>
2015-09-03 16:01:57 +0530

    ProductClaim Line of Business and Plan are taking from server

--- src/main/resources/static/js/productclaim/createproductclaim.js
+++ src/main/resources/static/js/productclaim/createproductclaim.js
@@ -12,11 +12,19 @@
         $scope.example1model = [];
         $scope.example1data = [{id: 1, label: "David"}, {id: 2, label: "Jhon"}, {id: 3, label: "Danny"}];
         $scope.benefitIds=[];
-
-
+        $scope.mapping={};
+        $scope.businessPlans=[];
         $scope.getPlan=function()
         {
-            alert('getPlan..');
+            //alert('getPlan..');
+            //alert($scope.mapping.lineOfBusiness);
+
+            //Call Http Method for getting Plan
+            $http.get('/pla/core/productclaimmap/getplanbylob/' + $scope.mapping.lineOfBusiness).success(function (response, status, headers, config) {
+                $scope.businessPlans=response;
+            }).error(function (response, status, headers, config) {
+            });
+
         }
         $scope.getCoverageClaimType=function()
         {
