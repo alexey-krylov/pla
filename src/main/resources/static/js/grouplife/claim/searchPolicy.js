72e72012891bb442eed1712eddffb8f883cdba8b
rudradash333@gmail.com <motherindia123>
2015-09-21 13:16:51 +0530

    search policy js file of claim module

--- src/main/resources/static/js/grouplife/claim/searchPolicy.js
+++ src/main/resources/static/js/grouplife/claim/searchPolicy.js
@@ -1,1 +1,40 @@
+var viewPolicyModule = (function () {
+    var services = {};
+    services.selectedItem = "";
+    services.status = '';
+    services.getTheItemSelected = function (ele) {
+        this.selectedItem = $(ele).val();
 
+        $(".btn-disabled").attr("disabled", false);
+
+    };
+
+    services.reload = function () {
+        window.location.reload();
+    };
+    services.printPolicy = function () {
+        var policyId = this.selectedItem;
+      //  window.location.href = "/pla/grouphealth/policy/viewpolicy?policyId=" + policyId  + "&mode=view";
+
+    };
+    services.emailPolicy = function () {
+        var policyId = this.selectedItem;
+      //  window.location.href = "/pla/grouphealth/policy/viewpolicy?policyId=" + policyId  + "&mode=view";
+
+    };
+    services.viewPolicy = function () {
+    alert(this.selectedItem);
+        var policyId = this.selectedItem;
+        window.location.href = "/pla/grouplife/claim/openclaimintimationpage/?policyId=" + policyId  + "&mode=view";
+
+    };
+     services.createPolicy = function () {
+           var policyId = this.selectedItem;
+
+           window.location.href ="/pla/grouplife/claim/openclaimintimationpage/?policyId=" + policyId + "&mode=edit";
+          //  window.location.href ="/pla/grouplife/claim/openclaimintimationpage";
+       };
+
+
+    return services;
+})();
