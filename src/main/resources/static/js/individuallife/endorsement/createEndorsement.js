f09d8a231cec980bffbbcb125db15deff6eb810f
prasant01 <prasant_nayak@nthdimenzion.com>
2015-10-21 18:00:16 +0530

    ILEndrosement Document Tab has Added and Change Payment Mode for Endorsement Type has added..

--- src/main/resources/static/js/individuallife/endorsement/createEndorsement.js
+++ src/main/resources/static/js/individuallife/endorsement/createEndorsement.js
@@ -29,10 +29,18 @@ app.config(["$routeProvider", function ($routeProvider) {
             $scope.genderList = globalConstants.gender;
             $scope.beneficiariesList = [];
             $scope.agentDetails = [];
-
+            $scope.bankDetailsResponse=[];
+            $scope.additionalDocumentList = [];
+            $scope.documentList = [];
             alert('createEndorsement');
             $http.get('/pla/core/master/getgeodetail').success(function (response, status, headers, config) {
                 $scope.provinces = response;
+            }).error(function (response, status, headers, config) {
+            });
+
+            $http.get('/pla/individuallife/endorsement/getAllBankNames').success(function (response, status, headers, config) {
+                $scope.bankDetailsResponse = response;
+                //console.log("Bank Details :"+JSON.stringify(response));
             }).error(function (response, status, headers, config) {
             });
 
@@ -161,6 +169,120 @@ app.config(["$routeProvider", function ($routeProvider) {
             $scope.newAgent={};
             $('#agentModal').modal('hide');
         };
+        $scope.deleteAgent=function(agentId){
+            for(var i=0;i< $scope.agentDetails.length;i++){
+                if($scope.agentDetails[i].agentId == agentId){
+                    $scope.agentDetails.splice(i,1);
+                }
+            }
+            $scope.commisionSumTest();
+        }
+
+        $scope.commisionSumTest = function () {
+            var sum = 0;
+            for (var i=0;i< $scope.agentDetails.length;i++) {
+
+                sum = parseFloat(sum) + parseFloat($scope.agentDetails[i].commission);
+            }
+            //console.log('sum: ' + sum);
+            if (sum == 100.00) {
+                $scope.agentMessage = false;
+            }
+            else {
+                $scope.agentMessage = true;
+            }
+        };
+
+        $scope.addAdditionalDocument = function () {
+            $scope.additionalDocumentList.unshift({});
+            $scope.checkDocumentAttached = $scope.isUploadEnabledForAdditionalDocument();
+
+        };
+
+        $scope.removeAdditionalDocument = function (index) {
+            $scope.additionalDocumentList.splice(index, 1);
+            $scope.checkDocumentAttached = $scope.isUploadEnabledForAdditionalDocument();
+        };
+
+        $scope.callAdditionalDoc = function (file) {
+            if (file[0]) {
+                $scope.checkDocumentAttached = $scope.isUploadEnabledForAdditionalDocument();
+            }
+        }
+        $scope.isUploadEnabledForAdditionalDocument = function () {
+            var enableAdditionalUploadButton = ($scope.additionalDocumentList != null);
+            for (var i = 0; i < $scope.additionalDocumentList.length; i++) {
+                var document = $scope.additionalDocumentList[i];
+                var files = document.documentAttached;
+                //alert(i+"--"+files)
+                //alert(i+"--"+document.content);
+                if (!(files || document.content)) {
+                    enableAdditionalUploadButton = false;
+                    break;
+                }
+            }
+            return enableAdditionalUploadButton;
+        }
+
+        $scope.documentNameLengthTest=function(){
+            var enableAdditionalUploadButton= true;
+            for(var i=0;i < $scope.documentList.length;i++){
+                if($scope.documentList[i].documentAttached != null && $scope.documentList[i].documentAttached[0] != null){
+                    //alert($scope.documentList[i].documentAttached[0].name.length);
+                    if($scope.documentList[i].documentAttached[0].name.length >100)
+                    {
+                        enableAdditionalUploadButton=false;
+                        break;
+                    }
+                }
+            }
+            if(enableAdditionalUploadButton){
+                 return true;
+            }
+            else
+            {
+                return false
+            }
+        }
+
+        $scope.documentList=[
+            {
+                "documentId":"BROUGHT_IN_DEAD_(BID)_CERTIFICATE",
+                "documentName":"BROUGHT IN DEAD(BID) CERTIFICATE",
+                "contentType":"application/x-javascript",
+                "gridFsDocId":"56040bfe7c8508839f7c0f56",
+                "mandatory":true,
+                "requireForSubmission":true,
+                "approved":true
+            },
+            {
+                "documentId":"ANY_CLINICAL_ABSTRACTS_RECORDS_IF_AVAILABLE",
+                "documentName":"ANY CLINICAL ABSTRACTS RECORDS IF AVAILABLE",
+                "contentType":"application/octet-stream",
+                "gridFsDocId":"56040bfe7c8508839f7c0f58",
+                "mandatory":true,
+                "requireForSubmission":true,
+                "approved":true
+            },
+            {
+                "documentId":"ADDRESS_PROOF",
+                "documentName":"ADDRESS PROOF",
+                "contentType":"application/octet-stream",
+                "gridFsDocId":"56040bff7c8508839f7c0f5a",
+                "mandatory":true,
+                "requireForSubmission":true,
+                "approved":true
+            },
+            {
+                "documentId":"BURIAL_CERTIFICATE",
+                "documentName":"BURIAL CERTIFICATE",
+                "contentType":"application/octet-stream",
+                "gridFsDocId":"56040bff7c8508839f7c0f5c",
+                "mandatory":true,
+                "requireForSubmission":true,
+                "approved":true
+            }
+        ];
 
     }])
 })(angular);
