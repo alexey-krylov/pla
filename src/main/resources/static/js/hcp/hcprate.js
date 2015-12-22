1712a31577cb5d046baf39fcc44b551885119bcf
Mohan Sharma <mohan_sharma@nthdimenzion.com>
2015-12-22 15:12:40 +0530

    js for hcprate

---
+++ src/main/resources/static/js/hcp/hcprate.js
@@ -0,0 +1,98 @@
+(function (angular) {
+    "use strict";
+    var app= angular.module('searchHcpRate', ['common', 'ngRoute', 'mgcrea.ngStrap.select', 'mgcrea.ngStrap.alert', 'mgcrea.ngStrap.popover',
+        'directives', 'mgcrea.ngStrap.dropdown', 'ngSanitize', 'commonServices','ui.bootstrap.modal','ngMessages','angularFileUpload']);
+    app.config(['datepickerPopupConfig', function (datepickerPopupConfig) {
+        datepickerPopupConfig.datepickerPopup = 'dd/MM/yyyy';
+        datepickerPopupConfig.currentText = 'Today';
+        datepickerPopupConfig.clearText = 'Clear';
+        datepickerPopupConfig.closeText = 'Done';
+        datepickerPopupConfig.closeOnDateSelection = true;
+    }])
+        .config(['$dropdownProvider', function ($dropdownProvider) {
+            angular.extend($dropdownProvider.defaults, {
+                html: true
+            });
+        }])
+        .config(["$routeProvider", function ($routeProvider) {
+            $routeProvider.when('/', {
+                    templateUrl: 'searchhcprate.html',
+                    controller: 'searchHcpRateController'
+                }
+            )}]);
+
+    app.controller('searchHcpRateController', ['$scope', '$http','$upload',  function ($scope, $http, $upload) {
+        $scope.uploadHCPServiceRatesDto = {};
+        $scope.showDownload = true;
+        $scope.fileSaved = null;
+        $scope.$watchCollection('[uploadHCPServiceRatesDto.hcpCode,showDownload]', function (n) {
+            if (n[0]) {
+                $scope.qId = n[0];
+                if (n[1]) {
+                    $scope.dropdown = [{
+                        "text": "<a><img src=\"/pla/images/xls-icon.png\">download</a>",
+                        "href": "/pla/core/hcp/downloadhcpratetemplate/" + $scope.qId
+                    }
+                    ];
+                } else {
+                    $scope.dropdown = [{
+                        "text": "<a><img src=\"/pla/images/xls-icon.png\">download</a>",
+                        "href": "/pla/core/hcp/downloadhcpratetemplate/" + $scope.hcpCode
+                    },{
+                        "text": "<a><img src=\"/pla/images/xls-icon.png\">Error File</a>",
+                        "href": "/pla/quotation/grouphealth/downloaderrorinsuredtemplate/" + $scope.qId
+                    }
+                    ];
+                }
+            }
+        });
+
+        $scope.$watch('fileSaved', function (n, o) {
+            if (n && n.length) {
+                $scope.fileName = n[0].name
+            }
+        });
+
+        $scope.uploadHCPRates = function () {
+            $scope.uploadHCPServiceRatesDto.fromDate = formatDate($scope.uploadHCPServiceRatesDto.fromDate);
+            $scope.uploadHCPServiceRatesDto.toDate = formatDate($scope.uploadHCPServiceRatesDto.toDate);
+            $upload.upload({
+                url: '/pla/quotation/grouphealth/uploadhcpratedetails',
+                headers: {'Authorization': 'xxx'},
+                fields: $scope.uploadHCPServiceRatesDto,
+                file: $scope.fileSaved
+            }).success(function (data, status, headers, config) {
+                if (data.status == "200") {
+                    if(data.id) {
+                        $scope.quotationId = data.id;
+                        $timeout($scope.updatePremiumDetail($scope.quotationId), 500);
+                        $http.get("/pla/quotation/grouphealth/isValidPremiumAndPerson/" + $scope.quotationId)
+                            .success(function (response) {
+                                console.log(response);
+                            });
+                    }
+                } else{
+                    if(data.data){
+                        $scope.showDownload = false;
+
+                    }else{
+                        $scope.showDownload = true;
+                    }
+                }
+            });
+        };
+
+    }])
+})(angular);
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
