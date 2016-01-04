554cfb7004b04c8be6f7c18208e9be78efa3899c
Mohan Sharma <mohan_sharma@nthdimenzion.com>
2016-01-04 10:52:33 +0530

    dtos for preauth upload

---
+++ src/main/java/com/pla/grouphealth/claim/cashless/presentation/dto/PreAuthorizationUploadDto.java
@@ -0,0 +1,30 @@
+package com.pla.grouphealth.claim.cashless.presentation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.joda.time.DateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by Mohan Sharma on 12/31/2015.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PreAuthorizationUploadDto {
+    @NotNull(message = "hcpCode must not be null")
+    @NotEmpty(message = "hcpCode must not be null")
+    private String hcpCode;
+    @NotNull(message = "file must not be null")
+    private MultipartFile file;
+    @NotNull(message = "fromDate must not be null")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private DateTime batchDate;
+}
