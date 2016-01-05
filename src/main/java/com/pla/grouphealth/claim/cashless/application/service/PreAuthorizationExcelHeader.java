d4bc0e4d4f7d42dca9900660c39a75510601a1e8
rudradash333@gmail.com <motherindia123>
2016-01-05 09:21:58 +0530

    duplicate for file uload

--- src/main/java/com/pla/grouphealth/claim/cashless/application/service/PreAuthorizationExcelHeader.java
+++ src/main/java/com/pla/grouphealth/claim/cashless/application/service/PreAuthorizationExcelHeader.java
@@ -1,5 +1,6 @@
 package com.pla.grouphealth.claim.cashless.application.service;
 
+import com.google.common.collect.Lists;
 import com.pla.grouphealth.claim.cashless.presentation.dto.PreAuthorizationDetailDto;
 import com.pla.grouphealth.policy.domain.model.GroupHealthPolicy;
 import com.pla.publishedlanguage.contract.IExcelPropagator;
@@ -7,7 +8,9 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.joda.time.DateTime;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -72,8 +75,10 @@ public enum PreAuthorizationExcelHeader {
                 Cell policyNumberCell = row.getCell(excelHeaders.indexOf(POLICY_NUMBER.name()));
                 String policyNumberValue = getCellValue(policyNumberCell);
                 GroupHealthPolicy groupHealthPolicy = iExcelPropagator.findPolicyByPolicyNumber(policyNumberValue);
-                //DateTime dateTime = groupHealthPolicy.getExpiredOn();
-               //if(dateTime.compareTo(new DateTime()) == 1 || dateTime.compareTo(new DateTime()) == 0)
+                DateTime  expiredOn= groupHealthPolicy.getExpiredOn();
+                if(expiredOn.toLocalDate().isAfter(new DateTime().toLocalDate())){
+                    errorMessage=errorMessage+  "Policy Is Expired";
+                }
 
             } catch (Exception e) {
                 errorMessage = errorMessage + e.getMessage();
@@ -1400,4 +1405,31 @@ public enum PreAuthorizationExcelHeader {
     public abstract PreAuthorizationDetailDto populatePreAuthorizationDetail(PreAuthorizationDetailDto preAuthorizationDetailDto, Row row, List<String> headers);
 
     public abstract String validateAndIfNotBuildErrorMessage(IExcelPropagator iExcelPropagator, Row row, String value, List<String> excelHeaders);
+
+    public static List<Row> findDuplicateRow(List<Row> dataRowsForDuplicateCheck, Row currentRow, List<String> headers) {
+        List<Row> duplicateRows = Lists.newArrayList();
+        Cell clientIdCell = currentRow.getCell(headers.indexOf(PreAuthorizationExcelHeader.CLIENT_ID.name()));
+        String clientIdCellValue = getCellValue(clientIdCell);
+        Cell serviceCell = currentRow.getCell(headers.indexOf(PreAuthorizationExcelHeader.SERVICE.name()));
+        String serviceCellValue = getCellValue(serviceCell);
+        Map<String,Object> currentRowNameRelationshipHolder = new HashMap<String,Object>();
+        currentRowNameRelationshipHolder.put("ClientID",clientIdCellValue);
+        currentRowNameRelationshipHolder.put("Service",serviceCellValue);
+
+        dataRowsForDuplicateCheck.forEach(dataRowForDuplicateCheck -> {
+            if (currentRow.getRowNum() != dataRowForDuplicateCheck.getRowNum()) {
+                Cell otherClientIdCell = currentRow.getCell(headers.indexOf(PreAuthorizationExcelHeader.CLIENT_ID.name()));
+                String otherClientIdValue = getCellValue(otherClientIdCell);
+                Cell otherServiceCell = currentRow.getCell(headers.indexOf(PreAuthorizationExcelHeader.SERVICE.name()));
+                String otherServiceValue = getCellValue(otherServiceCell);
+                Map<String,Object> otherRowNameRelationshipHolder = new HashMap<String,Object>();
+                currentRowNameRelationshipHolder.put("ClientID",otherClientIdValue);
+                currentRowNameRelationshipHolder.put("Service",otherServiceValue);
+                if (currentRowNameRelationshipHolder.equals(otherRowNameRelationshipHolder)) {
+                    duplicateRows.add(dataRowForDuplicateCheck);
+                }
+            }
+        });
+        return duplicateRows;
+    }
 }
