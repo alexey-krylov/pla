5b6cd25b3aac01c2ff4041eef41e0d0af8a971df
Mohan Sharma <mohan_sharma@nthdimenzion.com>
2016-01-12 18:24:04 +0530

    PreAuthorizationRequest modelling

--- src/main/resources/static/js/grouphealth/claim/preAuthorizationRequest.js
+++ src/main/resources/static/js/grouphealth/claim/preAuthorizationRequest.js
@@ -1,15 +1,15 @@
 (function (angular) {
     "use strict";
     var app= angular.module('CreatePreAuthorizationRequest', ['common', 'ngRoute', 'mgcrea.ngStrap.select', 'mgcrea.ngStrap.alert', 'mgcrea.ngStrap.popover', 'directives',
-                                                                  'angularFileUpload', 'mgcrea.ngStrap.dropdown', 'ngSanitize', 'commonServices']);
+        'angularFileUpload', 'mgcrea.ngStrap.dropdown', 'ngSanitize', 'commonServices']);
 
-        app.config(['datepickerPopupConfig', function (datepickerPopupConfig) {
-            datepickerPopupConfig.datepickerPopup = 'dd/MM/yyyy';
-            datepickerPopupConfig.currentText = 'Today';
-            datepickerPopupConfig.clearText = 'Clear';
-            datepickerPopupConfig.closeText = 'Done';
-            datepickerPopupConfig.closeOnDateSelection = true;
-        }])
+    app.config(['datepickerPopupConfig', function (datepickerPopupConfig) {
+        datepickerPopupConfig.datepickerPopup = 'dd/MM/yyyy';
+        datepickerPopupConfig.currentText = 'Today';
+        datepickerPopupConfig.clearText = 'Clear';
+        datepickerPopupConfig.closeText = 'Done';
+        datepickerPopupConfig.closeOnDateSelection = true;
+    }])
 
     app.config(["$routeProvider", function ($routeProvider) {
         $routeProvider.when('/', {
@@ -17,7 +17,7 @@
                 controller: 'createPreAuthorizationRequestCtrl',
                 resolve:{
                     createUpdateDto: ['$q', '$http', 'getQueryParameter', function ($q, $http, getQueryParameter) {
-                      var deferred = $q.defer();
+                        var deferred = $q.defer();
 
                         var preAuthorizationId = getQueryParameter('preAuthorizationId');
                         var clientId = getQueryParameter('clientId');
@@ -38,81 +38,74 @@
             }
         )}]);
 
-   app.controller('createPreAuthorizationRequestCtrl', ['$scope', '$http','createUpdateDto','$timeout','getQueryParameter','$window',
-                                                        function ($scope, $http,createUpdateDto, getQueryParameter,$window){
-          $scope.createUpdateDto = createUpdateDto;
-          $scope.drugServicesDtoList = $scope.createUpdateDto.drugServicesDtos;
-          $scope.treatmentDiagnosis={};
-          $scope.diagnosisTreatmentDtosUpdate={};
-          $scope.index = null;
-          $scope.isEditMode = false;
-          $scope.createUpdateCommand={};
-          $scope.diagnosisTreatmentDtos={};
-          $scope.createUpdateCommand.claimantHCPDetailDto=$scope.createUpdateDto.claimantHCPDetailDto;
-          $scope.createUpdateCommand.claimantHCPDetailDto={};
-          $scope.createUpdateCommand.claimantPolicyDetailDto={};
-          $scope.createUpdateCommand.diagnosisTreatmentDtos=[];
-          $scope.createUpdateCommand.illnessDetailDto={};
-          $scope.createUpdateCommand.drugServicesDtos=[];
+    app.controller('createPreAuthorizationRequestCtrl', ['$scope', '$http','createUpdateDto','$timeout','getQueryParameter','$window',
+        function ($scope, $http,createUpdateDto, getQueryParameter,$window){
+            $scope.createUpdateDto = createUpdateDto;
+            $scope.drugServicesDtoList = $scope.createUpdateDto.drugServicesDtos;
+            $scope.treatmentDiagnosis={};
+            $scope.diagnosisTreatmentDtoToUpdate={};
+            $scope.index = null;
+            $scope.treatmentDiagnosisIndex = null;
+            $scope.isEditMode = false;
+            $scope.createUpdateCommand={};
+            $scope.diagnosisTreatmentDtos={};
+            $scope.createUpdateCommand.claimantHCPDetailDto=$scope.createUpdateDto.claimantHCPDetailDto;
+            $scope.createUpdateCommand.claimantHCPDetailDto={};
+            $scope.createUpdateCommand.claimantPolicyDetailDto={};
+            $scope.createUpdateCommand.diagnosisTreatmentDtos=[];
+            $scope.createUpdateCommand.illnessDetailDto={};
+            $scope.createUpdateCommand.drugServicesDtos=[];
 
-          $scope.viewTreatmentDiagnosis = function(treatmentDiagnosis){
-          $scope.diagnosisTreatmentDtos = treatmentDiagnosis;
-          $scope.drugServicesDto = {};
-          $scope.diagnosisTreatmentDtosUpdate = {};
-          $scope.createUpdateDto.drugServicesDtosSave = [];
-         }
-
-         $scope.updatedrugServicesDtos = function(drugServicesDto,index){
-         $scope.index = index;
-         $scope.isEditMode = true;
-         $scope.diagnosisTreatmentDtosUpdate = drugServicesDto;
-         }
-
-
-
-          $scope.savediagnosisTreatmentDtos = function(diagnosisTreatmentDtosUpdate){
-
-           console.log("before - "+$scope.drugServicesDtoList);
-
-            if($scope.isEditMode){
-
-                $scope.createUpdateDto.drugServicesDtos[$scope.index] = diagnosisTreatmentDtosUpdate;
-
-                $scope.isEditMode = false;
-
-             } else{
-                $scope.drugServicesDtoList.push(diagnosisTreatmentDtosUpdate);
-               }
+            $scope.viewTreatmentDiagnosis = function(treatmentDiagnosis, treatmentDiagnosisIndex){
+                $scope.treatmentDiagnosisIndex = treatmentDiagnosisIndex;
+                $scope.diagnosisTreatmentDto = treatmentDiagnosis;
+                $scope.drugServicesDto = {};
+                $scope.diagnosisTreatmentDtosUpdate = {};
+                $scope.createUpdateDto.drugServicesDtosSave = [];
             }
 
+            $scope.updateTreatmentAndDiagnosis = function(diagnosisTreatmentDto){
+                $scope.createUpdateDto.diagnosisTreatmentDtos[$scope.treatmentDiagnosisIndex] = diagnosisTreatmentDto;
+            }
 
-              $scope.deleteTreatmentDiagnosis = function(drugServicesDto,index){
+            $scope.updateDrugServicesDto = function(drugServicesDto,index){
+                $scope.index = index;
+                $scope.isEditMode = true;
+                $scope.diagnosisTreatmentDtoToUpdate = drugServicesDto;
+            }
 
-               $scope.createUpdateDto.drugServicesDtos.splice(index, 1);
+            $scope.saveDiagnosisTreatmentDto = function(diagnosisTreatmentDtoToUpdate){
+                if($scope.isEditMode){
 
-               }
+                    $scope.createUpdateDto.drugServicesDtos[$scope.index] = diagnosisTreatmentDtoToUpdate;
 
+                    $scope.isEditMode = false;
 
-             $scope.saveClaimant= function(){
-                $scope.createUpdateCommand=$scope.createUpdateDto;
+                } else{
+                    $scope.drugServicesDtoList.push(diagnosisTreatmentDtoToUpdate);
+                }
+            }
+
+            $scope.deleteTreatmentDiagnosis = function(index){
+                $scope.createUpdateDto.drugServicesDtos.splice(index, 1);
+
+            }
+
+            $scope.savePreAuthorizationRequest= function(){
                 $http({
-                 url: '/pla/grouphealth/claim/cashless/preauthorizationrequest/createorupdate',
-                  method: 'POST',
-                   data: $scope.createUpdateCommand
+                    url: '/pla/grouphealth/claim/cashless/preauthorizationrequest/createorupdate',
+                    method: 'POST',
+                    data: $scope.createUpdateDto
                 }).then(function(response) {
-                   },
+                    },
                     function(response) {
-                });
-             }
-
-
-              $scope.back = function () {
-               window.location.reload();
+                    });
             }
 
-
-
-    }]);
+            $scope.back = function () {
+                window.location.reload();
+            }
+        }]);
 })(angular);
 
 function formatDate(date) {
